d15b4d96a9f21272acb596b192f4dded9c598f03
vl522 <vl522@ic.ac.uk>
2023-06-02 18:00:04 +0100

    Loading of Instructions into Array

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,38 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define NELEMENTS ((int) pow(2,18))
 
-int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
+int main( int argc, char **argv ) {
+    // Declaring Array to store binary instructions
+    long data[NELEMENTS];
+
+    if( argc != 2 ) {
+        fprintf( stderr, "Usage: ./emulate filename!\n" );
+        exit(1);
+    }
+
+    FILE *fp = fopen( argv[1], "rb" );
+
+    // Error check for opening file
+    if( fp == NULL ) {
+        fprintf( stderr, "cat: can’t open %s\n", argv[1] );
+        exit(1);
+    }
+
+    // Reading binary file
+    fread( data, sizeof(long), NELEMENTS, fp);
+
+    if( ferror(fp) ) {
+        fprintf( stderr, "Error occurred reading from output.txt\n" );
+        exit(1);
+    }
+
+    // Outputting contents of array storing binary file instructions
+    for (int i=0; i < NELEMENTS; i++) {
+        printf("%ld\n", data[i]);
+    }
+
+    return 0;
 }
+
