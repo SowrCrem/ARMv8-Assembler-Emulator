0dea0e09de403200da3ff7e17a0d3d689e3cdedb
Shlok Shah <shahshlok035@gmail.com>
2023-06-08 22:19:48 +0100

    Complete dataProcessingImmediates

--- src/dataProcessingImmediates.h
+++ src/dataProcessingImmediates.h
@@ -4,5 +4,8 @@
 
 #ifndef ARMV8_47_DATAPROCESSINGIMMEDIATES_H
 #define ARMV8_47_DATAPROCESSINGIMMEDIATES_H
+#include <stdint.h>
+
+void dataProcessingImmediateInstruction(uint32_t instr);
 
 #endif //ARMV8_47_DATAPROCESSINGIMMEDIATES_H
