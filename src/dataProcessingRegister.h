4dd14b4bab8521d5345e7023ce43a8f70ed73149
Saurajit Seth <ss3722@ic.ac.uk>
2023-06-08 22:09:18 +0100

    Update dataProcessingRegister.c (basic functionality)

--- src/dataProcessingRegister.h
+++ src/dataProcessingRegister.h
@@ -5,9 +5,4 @@
 #ifndef ARMV8_47_DATAPROCESSINGREGISTER_H
 #define ARMV8_47_DATAPROCESSINGREGISTER_H
 
-#endif //ARMV8_47_DATAPROCESSINGREGISTER_H
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdint.h>
-
-void chose(uint32_t instr);
+#endif //ARMV8_47_DATAPROCESSINGREGISTER_H#
