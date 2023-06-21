9e382a03af3d49f796dec10a5eef3470def6732c
Saurajit Seth <ss3722@ic.ac.uk>
2023-06-21 15:59:37 +0100

    Updated led_blink.s

--- src/led_blink.s
+++ src/led_blink.s
@@ -2,7 +2,7 @@
     .align 3
     .global gpio_base
 gpio_base:
-    .quad 0xFE200000   // Base address of GPIO registers
+    .quad 0x0000000000000000   // Base address of GPIO registers
 
 .section .text
     .global _start
@@ -10,12 +10,13 @@ _start:
     // Set GPIO pin 16 as an output
     ldr x0, =gpio_base
     ldr x1, [x0, #0x4]   // Read the current value of the GPFSEL1 register
-    bic x1, x1, #0xF0000 // Clear bits 18-20 to set as an output
-    orr x1, x1, #0x10000 // Set bit 16 to enable output on GPIO pin 16
+    bic x1, x1, #0x1C0000 // Clear bits 18-19 to set as an output
+    orr x1, x1, #0x140000 // Set bit 18 to enable output on GPIO pin 16
     str x1, [x0, #0x4]   // Write the updated value back to the GPFSEL1 register
 
 loop:
     // Turn on the LED
+    orr x1, x1, #0x10000 // Set bit 16 to turn on the LED
     str x1, [x0, #0x1C]  // Write a high value to the GPSET0 register (GPIO pin 16)
 
     mov x0, 1000000     // Wait for approximately 1 second
@@ -24,6 +25,7 @@ wait_loop:
     bne wait_loop
 
     // Turn off the LED
+    bic x1, x1, #0x10000 // Clear bit 16 to turn off the LED
     str x1, [x0, #0x28] // Write a high value to the GPCLR0 register (GPIO pin 16)
 
     mov x0, 1000000     // Wait for approximately 1 second
